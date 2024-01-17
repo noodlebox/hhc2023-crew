84b7dafbc3f31d060b5f586857dd652273980fe0
noodlebox <ash@noodlebox.moe>
2024-01-17 13:33:54 -0500

    Bump version to 1.4

--- crew.user.js
+++ crew.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Captain and Crew (Holiday Hack Challenge 2023)
 // @namespace   https://files.noodlebox.moe/
-// @version     1.3
+// @version     1.4
 // @description Various features and QoL fixes for the sailing mode of Holiday Hack Challenge 2023.
 // @author      noodlebox
 // @match       https://2023.holidayhackchallenge.com/sea/*
